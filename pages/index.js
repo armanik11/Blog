52fafb58cd0061542221866443e70d5670c02069
Arunkumar Manikantan <armanika@microsoft.com>
2021-02-17 14:52:22 -0800

    Fixing titles

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Arunkumar Manikantan. I'm a software engineer working in Microsoft.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Hello, I'm Arunkumar Manikantan. I'm a software engineer working in Microsoft.</p>      
       </section>
     </Layout>
   )
